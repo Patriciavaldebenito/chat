668264584fe7925efaa2d696e50d847c04e9d9a2
Patriciavaldebenito <patricia.vpanes@gmail.com>
2020-03-11 12:28:37 -0300

    first commit, creación de proyecto, y creacion de componentes : home, login, profile y conversation. Configuración de rutas en app.module y app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { ConversationComponent } from './components/conversation/conversation.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'',   component:HomeComponent},
+  { path:'home',   component:HomeComponent},
+  { path:'login', component:LoginComponent},
+
+  { path:'conversation',   component:ConversationComponent},
+  { path:'profile',   component:ProfileComponent},
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
